4cb363e13ccad7fb5963cab15381d9799f12ee50
determinedguy <mhd.athallah@gmail.com>
2021-05-13 20:28:35 +0700

    Perfects the code and some parts

--- codelab/lib/detail_screen.dart
+++ codelab/lib/detail_screen.dart
@@ -197,130 +197,135 @@ class _DetailWebPageState extends State<DetailWebPage> {
           vertical: 16,
           horizontal: 64,
         ),
-        child: Center(
-          child: Container(
-            width: screenWidth <= 1200 ? 800 : 1200,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Wisata Bandung',
-                  style: TextStyle(
-                    fontFamily: 'Staatliches',
-                    fontSize: 32,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: screenWidth <= 1200 ? 800 : 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Wisata Bandung',
+                    style: TextStyle(
+                      fontFamily: 'Staatliches',
+                      fontSize: 32,
+                    ),
                   ),
-                ),
-                SizedBox(height: 32),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          ClipRRect(
-                            child: Image.asset(widget.place.imageAsset),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          SizedBox(height: 16),
-                          Scrollbar(
-                            isAlwaysShown: true,
-                            controller: _scrollController,
-                            child: Container(
-                              height: 150,
-                              padding: const EdgeInsets.only(bottom: 16),
-                              child: ListView(
-                                controller: _scrollController,
-                                scrollDirection: Axis.horizontal,
-                                children: widget.place.imageUrls.map((url) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: Image.network(url),
-                                    ),
-                                  );
-                                }).toList(),
+                  SizedBox(height: 32),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              child: Hero(
+                                tag: 'image_' + widget.place.name,
+                                child: Image.asset(widget.place.imageAsset),
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            SizedBox(height: 16),
+                            Scrollbar(
+                              isAlwaysShown: true,
+                              controller: _scrollController,
+                              child: Container(
+                                height: 150,
+                                padding: const EdgeInsets.only(bottom: 16),
+                                child: ListView(
+                                  controller: _scrollController,
+                                  scrollDirection: Axis.horizontal,
+                                  children: widget.place.imageUrls.map((url) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(url),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(width: 32),
-                    Expanded(
-                      child: Card(
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Container(
-                                child: Text(
-                                  widget.place.name,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 30.0,
-                                    fontFamily: 'Staatliches',
+                      SizedBox(width: 32),
+                      Expanded(
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Container(
+                                  child: Text(
+                                    widget.place.name,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontFamily: 'Staatliches',
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: <Widget>[
-                                      Icon(Icons.calendar_today),
-                                      SizedBox(width: 8.0),
-                                      Text(
-                                        widget.place.openDays,
-                                        style: informationTextStyle,
-                                      ),
-                                    ],
-                                  ),
-                                  FavoriteButton(),
-                                ],
-                              ),
-                              Row(
-                                children: <Widget>[
-                                  Icon(Icons.access_time),
-                                  SizedBox(width: 8.0),
-                                  Text(
-                                    widget.place.openTime,
-                                    style: informationTextStyle,
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 8.0),
-                              Row(
-                                children: <Widget>[
-                                  Icon(Icons.monetization_on),
-                                  SizedBox(width: 8.0),
-                                  Text(
-                                    widget.place.ticketPrice,
-                                    style: informationTextStyle,
-                                  ),
-                                ],
-                              ),
-                              Container(
-                                padding: EdgeInsets.symmetric(vertical: 16.0),
-                                child: Text(
-                                  widget.place.description,
-                                  textAlign: TextAlign.justify,
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontFamily: 'Oxygen',
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: <Widget>[
+                                        Icon(Icons.calendar_today),
+                                        SizedBox(width: 8.0),
+                                        Text(
+                                          widget.place.openDays,
+                                          style: informationTextStyle,
+                                        ),
+                                      ],
+                                    ),
+                                    FavoriteButton(),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(Icons.access_time),
+                                    SizedBox(width: 8.0),
+                                    Text(
+                                      widget.place.openTime,
+                                      style: informationTextStyle,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 8.0),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(Icons.monetization_on),
+                                    SizedBox(width: 8.0),
+                                    Text(
+                                      widget.place.ticketPrice,
+                                      style: informationTextStyle,
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                                  child: Text(
+                                    widget.place.description,
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontFamily: 'Oxygen',
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
