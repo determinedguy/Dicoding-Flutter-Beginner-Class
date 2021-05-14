c0b6f7e69de7dbd9d467053511b9df3d8528e48e
determinedguy <mhd.athallah@gmail.com>
2021-05-14 19:46:36 +0700

    Work on basic things (passed name, mobile-first)

--- final_project/lib/main_screen.dart
+++ final_project/lib/main_screen.dart
@@ -7,31 +7,104 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: Text('Get Up and Go!'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.person,
+                color: Colors.black87,
+              ),
+              tooltip: 'About Me',
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return AboutMeScreen();
+                }));
+              }),
+        ],
       ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth <= 600) {
+          return InputMobile();
+          /*if (constraints.maxWidth <= 600) {
             return InputMobile();
-          } else {
-            return InputWeb();
           }
+          else {
+            return InputWeb();
+          }*/
         },
       ),
     );
   }
 }
 
-class InputMobile extends StatelessWidget {
+class InputMobile extends StatefulWidget {
+  @override
+  _InputMobileState createState() => _InputMobileState();
+}
+
+class _InputMobileState extends State<InputMobile> {
+  TextEditingController _controller = TextEditingController();
+  String name;
+
   @override
   Widget build(BuildContext context) {
-    return;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Ketik namamu disini :)',
+                labelText: 'Nama',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Motivate Yourself!'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightGreen,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              ),
+              /*
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('Hello, ${_controller.text}'),
+                      );
+                    });
+              },
+              */
+              onPressed: () {
+                name = _controller.text;
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MotivationScreen(name: name);
+                }));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
 
+/*
 class InputWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return;
   }
 }
+*/
--- final_project/lib/motivation_screen.dart
+++ final_project/lib/motivation_screen.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MotivationScreen extends StatelessWidget {
+  final String name;
+
+  MotivationScreen({this.name});
+
   @override
   Widget build(BuildContext context) {
-    return;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Halo, ${this.name}.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  //fontFamily: 'Oxygen',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
